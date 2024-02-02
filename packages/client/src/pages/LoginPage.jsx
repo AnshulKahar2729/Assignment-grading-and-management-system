a4e2eb687830de08eeb46d7df880e1ef06826e2d
Pranav-Kale <pranavkale1124@gmail.com>
2024-02-02 18:05:57 +0530

    Login and signup page UI

--- packages/client/src/pages/LoginPage.jsx
+++ packages/client/src/pages/LoginPage.jsx
@@ -1,11 +1,74 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+import Login from '../assets/Login.gif' ;
+import Logo from '../assets/EduTrack (2).png' ;
 
 const LoginPage = () => {
-  return (
-    <div>
-      LOGINPAGE
-    </div>
-  )
-}
+    return (
+      <div className="flex flex-col pt-16 md:pt-0 md:flex-row h-screen items-center">
+        <div className="md:flex-1 bg-white p-8 flex flex-col items-center justify-center gap-6">
+          <div className="max-w-md w-full ">
+            <Link href="/">
+              <img src={Logo} className="h-20" alt="none" />
+            </Link>
+            <div className='pl-2'>
+            <h2 className="text-3xl font-extrabold mb-4 text-[#176DEF]">Login</h2>
+            <form>
+              <div className="mb-4">
+                <label className="block text-sm font-medium">Email</label>
+                <input
+                  type="email"
+                  className="w-full border p-2 rounded bg-gray-100 outline-none"
+                  required
+                />
+              </div>
+              <div className="mb-4">
+                <label className="block text-sm font-medium">Password</label>
+                <input
+                  type="password"
+                  className="w-full border p-2 rounded bg-gray-100 outline-none text-[#176DEF]"
+                  required
+                />
+              </div>
+              <div className="mt-5">
+                  <label htmlFor="role">Role -</label>
+                  <select
+                    id="role"
+                    className="w-full px-4 py-2 mb-2 border  bg-gray-100 rounded-md"
+                  >
+                    <option value="student">Student</option>
+                    <option value="teacher">Teacher</option>
+                  </select>
+              </div>
+
+              <button
+                type="submit"
+                className="mt-8 bg-[#176DEF] text-white py-3 px-8 rounded-lg font-semibold hover:bg-[#124a9e] focus:outline-none focus:shadow-outline-black"
+              >
+                Login
+              </button>
+            </form>
+            <p className="mt-4 text-sm">
+              Don't have an account?{" "}
+              <Link href="/">
+                <span className="text-[#176DEF]">Sign up here</span>
+              </Link>
+            </p>
+            </div>
+          </div>
+        </div>
+
+        {/* Right Section (Image) */}
+        <div className=" w-full md:flex-1 bg-white hidden  md:flex h-screen justify-center items-center p-10">
+        <img
+            src={Login}
+            alt="Register Image"
+            className="object-cover lg:w-[450px] lg:h-[450px] xl:h-[99%] xl:w-auto"
+        />
+      </div>
+      </div>
+    );
+  }
+  
 
 export default LoginPage
--- packages/client/src/pages/RegisterPage.jsx
+++ packages/client/src/pages/RegisterPage.jsx
@@ -1,11 +1,140 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom';
+import Logo from '../assets/EduTrack (2).png';
+import Register from '../assets/Sign up.gif';
 
-const RegisterPage = () => {
+function RegisterPage() {
+
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [role, setRole] = useState("teacher");
+    const [year, setYear] = useState("");
+    const [branch, setBranch] = useState("");
+    const [division, setDivision] = useState("");
+    const navigate = useNavigate();
+
+   const handleSignup = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div>
-      REGISTERPAGE
+    <div className="relative flex flex-col md:flex-row pt-4 sm:pt-16 md:pt-0 h-full">
+      <div className="md:flex-1 bg-white flex flex-col items-center justify-center p-4">
+        <div className="max-w-md w-full">
+          <Link href='/'>
+            <img src={Logo} className='h-20 ' alt="none" />
+          </Link>
+          <div className='m-2'>
+          <h2 className="text-3xl font-extrabold mb-4 text-[#176DEF]">Sign Up</h2>
+          <form onSubmit={handleSignup}>
+            {/* Add your signup form fields here */}
+            <div className="mb-4">
+              <label className="block text-sm font-medium">Full Name</label>
+              <input
+                type="text"
+                className="w-full border p-2 rounded bg-gray-100 text-black outline-none"
+                required
+              />
+            </div>
+            <div className="mb-4">
+              <label className="block text-sm font-medium">Email</label>
+              <input
+                type="email"
+                className="w-full border p-2 rounded bg-gray-100 text-black outline-none"
+                required
+              />
+            </div>
+            <div className="mb-4 ">
+              <label className="block text-sm font-medium ">Password</label>
+              <input
+                type="password"
+                className="w-full border p-2 rounded bg-gray-100 text-black outline-none"
+                required
+              />
+            </div>
+
+            <div className="mt-5">
+                  <label htmlFor="role">Role -</label>
+                  <select
+                    id="role"
+                    value={role}
+                    onChange={(event) => {
+                      setRole(event.target.value);
+                    }}
+                    className="w-full px-4 py-2 mb-2 border border bg-gray-100 rounded-md outline-none"
+                  >
+                    <option className='' value="teacher">Teacher</option>
+                    <option className='' value="student">Student</option>
+                  </select>
+                </div>
+                {/* added fields */}
+                {role === "student" &&
+                <>
+                  {/* Year  */}
+                  <label htmlFor="year" className="block mb-2 mt-5">
+                    Year
+                  </label>
+                  <input
+                    value={year}
+                    onChange={(event) => {
+                      setYear(event.target.value);
+                    }}
+                    className="w-full px-4 py-2 mb-2 border border-gray-400 rounded-md"
+                    type="text"
+                    id="year"
+                    placeholder="Your Year"
+                  />
+                  {/* branch  */}
+                  <label htmlFor="branch" className="block mb-2 mt-5">
+                    Branch
+                  </label>
+                  <input
+                    value={branch}
+                    onChange={(event) => {
+                      setBranch(event.target.value);
+                    }}
+                    className="w-full px-4 py-2 mb-2 border border-gray-400 rounded-md"
+                    type="text"
+                    id="branch"
+                    placeholder="Your Branch"
+                  />
+                  {/* division  */}
+                  <label htmlFor="division" className="block mb-2 mt-5">
+                    Division
+                  </label>
+                  <input
+                    value={division}
+                    onChange={(event) => {
+                      setDivision(event.target.value);
+                    }}
+                    className="w-full px-4 py-2 mb-2 border border-gray-400 rounded-md"
+                    type="text"
+                    id="division"
+                    placeholder="Your Division"
+                  />
+                </>
+                }
+            <button
+              type="submit"
+              className="mt-12 bg-[#176DEF] text-white py-3 px-8 rounded-lg font-semibold hover:bg-[#124a9e] focus:outline-none focus:shadow-outline-black"
+            >
+              Sign Up
+            </button>
+          </form>
+          </div>
+        </div>
+      </div>
+
+      <div className=" w-full md:flex-1 bg-white hidden  md:flex h-screen justify-center items-center p-10">
+        <img
+            src={Register}
+            alt="Register Image"
+            className="object-cover lg:w-[450px] lg:h-[450px] xl:h-[99%] xl:w-auto"
+        />
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default RegisterPage
