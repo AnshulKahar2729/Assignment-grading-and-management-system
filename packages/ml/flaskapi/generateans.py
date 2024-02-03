1d8767c724f25336942207d4ddb40a46bd1b5f3c
Anshul Kahar <22511126.dypit@dypvp.edu.in>
2024-02-03 23:39:01 +0530

    genaiapi

--- packages/ml/flaskapi/generateans.py
+++ packages/ml/flaskapi/generateans.py
@@ -8,19 +8,12 @@ GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
 # Configure the Generative AI API with the provided key
 genai.configure(api_key=GOOGLE_API_KEY)
 
-def shorten_response(response, max_length=100):
-    return response.text[:max_length]
-
 # Choose a model (make sure 'gemini-pro' is a valid model)
 model = genai.GenerativeModel('gemini-pro')
 
 def generateGenAns(assignmentCleanedTextArrElement):
     response = model.generate_content(assignmentCleanedTextArrElement)
-    # Shorten the response
-    shortened_response = shorten_response(response, max_length=100)
-    # Print the shortened response as plain text
-    print(shortened_response)
-    return shortened_response
+    return response
 
 
 
