a3bff6fb45d0e787a7bf04dd5fe0eb5c0596064b
Anshul Kahar <22511126.dypit@dypvp.edu.in>
2024-02-03 14:06:26 +0530

    bug fixes

--- packages/server/routes/student.js
+++ packages/server/routes/student.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Student = require("../models/student");
+const Student = require("../models/Student");
 
 router.get("/", async(req, res) => {
     try{
