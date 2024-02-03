2ff1064a5d677087a06d56a6c1801a57a5e4ee23
vikas-saini-7 <vikas289888@gmail.com>
2024-02-03 14:46:43 +0530

    Frontend deployment

--- packages/client/src/index.js
+++ packages/client/src/index.js
@@ -11,11 +11,11 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <Provider store={store}> */}
-      <UserProvider>
         <BrowserRouter>
-          <App />
+          <UserProvider>
+              <App />
+          </UserProvider>
         </BrowserRouter>
-      </UserProvider>
     {/* </Provider> */}
   </React.StrictMode>
 );
